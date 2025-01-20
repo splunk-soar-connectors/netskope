5698d549344e3261828072897a5831c0a247fce9
Crest Data <60967033+crestdatasystems@users.noreply.github.com>
2025-01-20 19:11:36 +0530

    Netskope: Bug Fix: Bug fixes in add and update url list actions (#14)
    * Fixed url list
    
    * Update README.md
    
    * Added pre-commit changes
    
    * Fixed linting errors
    
    * Added pre-commit changes
    
    * Fixed pipelines failures
    
    * Fixed linting errors
    
    * Fixed inconsistency variable errors
    
    * Fixed linting errors
    
    * Updated message variables
    
    * Updated error variables
    
    * removed requests library
    
    ---------
    
    Co-authored-by: splunk-soar-connectors-admin <admin@splunksoar>
    Co-authored-by: Ishan Shah <ishans@splunk.com>

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # File: __init__.py
 #
-# Copyright 2018-2022 Netskope, Inc.
+# Copyright 2018-2025 Netskope, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
